c05b4ada4a7f3544dd55d508c32ab99043324bcd
FichtnerTobias <tobias.fichtner.024@pslib.cz>
2025-04-16 15:58:25 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,34 @@
+let min = 0
+let max = 100
+let guess = Math.round((min + max) / 2)
+let pokusy = 1
+basic.showNumber(guess)
 
+while (true) {
+    if (input.buttonIsPressed(Button.A)) {
+
+        // Tvoje číslo je menší
+
+        max = guess - 1
+        guess = Math.round((min + max) / 2)
+        pokusy += 1
+        basic.showNumber(guess)
+
+    } else if (input.buttonIsPressed(Button.B)) {
+
+        // Tvoje číslo je větší
+
+        min = guess + 1
+        guess = Math.round((min + max) / 2)
+        pokusy += 1
+        basic.showNumber(guess)
+
+    } else if (input.buttonIsPressed(Button.AB)) {
+
+        // Uhodl jsem!
+
+        basic.showString("Pokusy:")
+        basic.showNumber(pokusy)
+        break
+    }
+}
